109a2dff86ab543f58dea49cee80da0129298e8d
Tasanakorn Phaipool <tasanakorn@gmail.com>
2013-05-22 15:15:58 +0700

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple Raspberry Pi MMAL project
+
+Build
+-----
+1. place  Raspberry Pi userland project /home/pi/src/raspberrypi/userland
+2. run
+    $ mkdir build
+    $ cd build
+    $ cmake
+    $ make 
---
+++ main.c
@@ -0,0 +1,118 @@
+/* 
+ * File:   main.c
+ * Author: tas
+ *
+ * Created on May 22, 2013, 1:52 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bcm_host.h"
+#include "interface/vcos/vcos.h"
+
+#include "interface/mmal/mmal.h"
+#include "interface/mmal/util/mmal_default_components.h"
+#include "interface/mmal/util/mmal_connection.h"
+
+#define MMAL_CAMERA_PREVIEW_PORT 0
+#define MMAL_CAMERA_VIDEO_PORT 1
+#define MMAL_CAMERA_CAPTURE_PORT 2
+
+int main(int argc, char** argv) {
+    MMAL_COMPONENT_T *camera = 0;
+    MMAL_COMPONENT_T *preview = 0;
+    MMAL_ES_FORMAT_T *format;
+    MMAL_STATUS_T status;
+    MMAL_PORT_T *camera_preview_port = NULL, *camera_video_port = NULL, *camera_still_port = NULL;
+    MMAL_PORT_T *preview_input_port = NULL;
+
+    MMAL_CONNECTION_T *camera_preview_connection = 0;
+
+    printf("Running...\n");
+
+
+    bcm_host_init();
+
+    status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera);
+    if (status != MMAL_SUCCESS) {
+        printf("Error: create camera %x\n", status);
+    }
+    printf("  Number of camera output port = %d\n", camera->output_num);
+
+    camera_preview_port = camera->output[MMAL_CAMERA_PREVIEW_PORT];
+    camera_video_port = camera->output[MMAL_CAMERA_VIDEO_PORT];
+    camera_still_port = camera->output[MMAL_CAMERA_CAPTURE_PORT];
+
+    {
+        MMAL_PARAMETER_CAMERA_CONFIG_T cam_config = {
+            { MMAL_PARAMETER_CAMERA_CONFIG, sizeof (cam_config)},
+            .max_stills_w = 1280,
+            .max_stills_h = 720,
+            .stills_yuv422 = 0,
+            .one_shot_stills = 1,
+            .max_preview_video_w = 1280,
+            .max_preview_video_h = 720,
+            .num_preview_video_frames = 3,
+            .stills_capture_circular_buffer_height = 0,
+            .fast_preview_resume = 0,
+            .use_stc_timestamp = MMAL_PARAM_TIMESTAMP_MODE_RESET_STC
+        };
+        mmal_port_parameter_set(camera->control, &cam_config.hdr);
+    }
+
+    format = camera_preview_port->format;
+
+    format->encoding = MMAL_ENCODING_OPAQUE;
+    format->encoding_variant = MMAL_ENCODING_I420;
+
+    format->es->video.width = 1280;
+    format->es->video.height = 720;
+    format->es->video.crop.x = 0;
+    format->es->video.crop.y = 0;
+    format->es->video.crop.width = 1280;
+    format->es->video.crop.height = 720;
+
+    status = mmal_port_format_commit(camera_preview_port);
+
+    if (status != MMAL_SUCCESS) {
+        printf("Error: camera viewfinder format couldn't be set\n");
+    }
+
+    status = mmal_component_enable(camera);
+
+    status = mmal_component_create(MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER, &preview);
+    if (status != MMAL_SUCCESS) {
+        printf("Error: unable to create preview (%u)\n", status);
+    }
+    preview_input_port = preview->input[0];
+
+    {
+        MMAL_DISPLAYREGION_T param;
+        param.hdr.id = MMAL_PARAMETER_DISPLAYREGION;
+        param.hdr.size = sizeof (MMAL_DISPLAYREGION_T);
+        param.set = MMAL_DISPLAY_SET_LAYER;
+        param.layer = 0;
+        param.set |= MMAL_DISPLAY_SET_FULLSCREEN;
+        param.fullscreen = 1;
+        status = mmal_port_parameter_set(preview_input_port, &param.hdr);
+        if (status != MMAL_SUCCESS && status != MMAL_ENOSYS) {
+            printf("Error: unable to set preview port parameters (%u)\n", status);
+        }
+    }
+
+    status = mmal_connection_create(&camera_preview_connection, camera_preview_port, preview_input_port, MMAL_CONNECTION_FLAG_TUNNELLING | MMAL_CONNECTION_FLAG_ALLOCATION_ON_INPUT);
+    if (status != MMAL_SUCCESS) {
+        printf("Error: unable to create connection (%u)\n", status);
+    }
+
+    status = mmal_connection_enable(camera_preview_connection);
+    if (status != MMAL_SUCCESS) {
+        printf("Error: unable to enable connection (%u)\n", status);
+    }
+    
+    while (1);
+
+    return (EXIT_SUCCESS);
+}
+
